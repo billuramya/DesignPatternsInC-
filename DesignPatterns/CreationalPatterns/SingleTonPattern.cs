edf4563d34907d765f2c427c34841a493d9b93b2
FIRST_NAME <billuramya2002@gamil.com>
2024-03-13 10:27:27 +0530

    DesignPatterns

---
+++ DesignPatterns/CreationalPatterns/SingleTonPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns
+{
+    class SingleTonPattern
+    {
+        private static int counter = 0;
+        public SingleTonPattern()
+        {
+            counter++;
+            Console.WriteLine("Counter Value {0}", counter);
+        }
+
+            public void PrintDetails(string Message)
+            {
+                Console.WriteLine(Message);
+            }
+        
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.CreationalPatterns;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SingleTonPattern s=new SingleTonPattern();
+        s.PrintDetails("This is my first Object Creation");
+        //s.PrintDetails("This is my second");
+        SingleTonPattern employee = new SingleTonPattern();
+        s.PrintDetails("Employee Object");
+        
+
+    }
+    
+}
